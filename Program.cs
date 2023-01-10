e34c4200e2537d709f3e18a41769902b4367346b
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2023-01-10 19:11:04 -0300

    add for loop and date exercises

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine(FahrenheitToCelsius(100));
             Console.WriteLine(GetSubstring("Hello World", 0, 5));
             Console.WriteLine(RepeatWord("a", 30));
+            Console.WriteLine(GetDaysFromDate(new DateTime(2000, 1, 1)));
+            Console.WriteLine(GetSumFromRange(1, 99));
         }
 
         public static int FahrenheitToCelsius(int fahrenheit)
@@ -22,6 +24,23 @@
         public static string RepeatWord(string word, int times)
         {
             return String.Concat(Enumerable.Repeat(word, times));
+        }
+
+        public static string GetDaysFromDate(DateTime date)
+        {
+            return $"{(DateTime.Now - date).Days} have passed since {date.ToString("dd/MM/yyyy")}.";
+        }
+
+        public static int GetSumFromRange(int start, int end)
+        {
+            int result = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                result += i;
+            }
+
+            return result;
         }
     }
 }
